ac2725955ed8308921f58904b1c9c992fd495b9c
Giacomo Scarabicchi <pinkpallyn@gmail.com>
2023-01-24 14:34:41 +0000

    Not working actually
    Change-Id: I01dc956b7f6ed9676677e1cab87a92aa37bd34d1

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ with open(r'sub_file.txt', 'r') as file:
     data = file.readlines()
 
 for index, item in enumerate(data):
-    print(index)
+
+    print(index+1)
+    # changed_string = string.replace("b", "B")
+    item = item.replace("[", "")
+    item = item.replace("]", "")
+    item = item.replace("  ", "\n")
     print(item)
